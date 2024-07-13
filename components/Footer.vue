545b3b47f0232995a4bb540a62072c75c6622cd8
Leo Mihalcea <leomihalcea@gmail.com>
2024-07-13 14:47:02 +0300

    feat: implemented footer and header

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const version = config.public['clientVersion'];
+</script>
+
 <template>
-Footer
+    <footer>
+        <Card>
+            <template #content>
+                <p>&copy; {{ $t('copyright') }}</p>
+                <p class="text-color-secondary">v{{ version ?? " 0.0.1" }}</p>
+            </template>
+        </Card>
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    text-align: center;
+}
+
+.p-card {
+    background: none;
+}
+
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,61 @@
+<script setup lang="ts">
+    import type {MenuItem} from 'primevue/menuitem';
+
+    const {locale, t} = useI18n();
+    let localePath = useLocalePath();
+
+    const items = ref<MenuItem[]>([]);
+
+    watch(() => locale.value, () => {
+        updateItems();
+    });
+
+    const updateItems = () => {
+        items.value = [
+            {
+                label: t('home'),
+                url: localePath('/'),
+            },
+            {
+                label: t('team'),
+                url: localePath('/team'),
+            },
+            {
+                label: t('about'),
+                url: localePath('/about'),
+            },
+            {
+                label: t('articles'),
+                url: localePath('/articles'),
+            },
+            {
+                label: t('gallery'),
+                url: "https://www.flickr.com/photos/200576225@N02/albums",
+            },
+        ];
+    };
+
+    updateItems();
+</script>
+
 <template>
-Header
+    <header>
+        <Menubar :model="items">
+        </Menubar>
+    </header>
 </template>
+
+<style scoped>
+    .p-menubar {
+        background: none;
+        border: none;
+        flex-direction: row-reverse;
+        position: relative;
+    }
+
+    /* todo(leo): re-enable deep selector once design is done */
+    .p-menubar-root-list{
+        background: #041936;
+    }
+
+</style>
