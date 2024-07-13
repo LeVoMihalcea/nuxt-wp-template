ebabf0fe5ed44ed54aff757f6b0b810bd47f34f9
Leo Mihalcea <leomihalcea@gmail.com>
2024-07-13 14:49:22 +0300

    feat: added extra pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,9 @@
 <script setup lang="ts">
-const route = useRoute()
-const config = useRuntimeConfig();
-const {data, refresh, pending} = await useFetch(config.public.wordpressUrl, {
-    method: 'get',
-    query: {
-        query: `
-     query NewQuery {
-       posts(first:10){
-         nodes {
-           title
-           date
-           excerpt
-           uri
-         }
-       }
-     }`
-    },
-    transform(data) {
-        return data.data.posts.nodes as Array<Record<'title' | 'date' | 'excerpt' | 'uri', string>>;
-    }
-});
 </script>
 
 
 <template>
-    Main page
-    <div class="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6">
-        <Post v-for="post in data" :key="post.uri" :post="post"></Post>
-    </div>
+    Main
 </template>
 
 <style scoped></style>
---
+++ pages/team.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Team</h1>
+    <div class="grid">
+        <div class="col-12 md:col-6 lg:col-4" v-for="member in data">
+            <team-member-card
+                :name=member.title?.rendered
+                :picture="member._embedded['wp:featuredmedia'][0]?.link"
+                department="random"
+                title="some else"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const data: Ref<any> = ref(null);
+onMounted(async () => {
+    data.value = await $fetch('/api/staff?_embed');
+});
+</script>
+
+
+<style scoped></style>
+	
