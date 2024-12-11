8a30918390134201cef66d19fd44785d78f4f29d
Leo Mihalcea <leomihalcea@gmail.com>
2024-12-11 21:55:44 +0200

    style: styled buttons in navbar

--- components/Header.vue
+++ components/Header.vue
@@ -48,10 +48,12 @@
     <header>
         <Menubar :model="items" class="md:h-5rem md:pr-8 flex flex-row justify-content-between menubar">
             <template #start>
-                <nuxt-img src="icon.svg" class="h-2rem"/>
+                <nuxt-img src="icon.svg" class="h-2rem lg:ml-7 ml-1 mt-1"/>
             </template>
             <template #item="{ item, props, hasSubmenu }">
-                <a :href="item.url">{{ item.label }}</a>
+                <a :href="item.url" class="hover:bg-red-50">
+                    <Button :label="item.label" variant="link" class="text-white"/>
+                </a>
             </template>
         </Menubar>
     </header>
@@ -60,13 +62,5 @@
 <style scoped>
     .p-menubar {
         background: linear-gradient(to right, color-mix(in srgb, var(--dark-blue), transparent 0%), color-mix(in srgb, var(--dark-blue), transparent 20%));
-    }
-
-    :deep(.p-menubar-root-list) {
-        background: var(--dark-blue);
-    }
-
-    a {
-        text-decoration: none;
     }
 </style>
