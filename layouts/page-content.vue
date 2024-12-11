975492a84c4f5ad0b62744727e716aad96458ba7
Leo Mihalcea <leomihalcea@gmail.com>
2024-12-11 21:55:11 +0200

    style: polish the page content

--- layouts/page-content.vue
+++ layouts/page-content.vue
@@ -3,14 +3,13 @@
 </script>
 
 <template>
-    <section class="page-content">
+    <section class="page-content md:m-8 m-3">
         <slot></slot>
     </section>
 </template>
 
 <style scoped>
     section {
-        margin: auto;
         max-width: 1600px;
     }
 </style>
