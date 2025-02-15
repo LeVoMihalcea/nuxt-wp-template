3076102724928a8185128302007ba130b6073681
Leo Mihalcea <leomihalcea@gmail.com>
2025-02-15 11:02:56 +0200

    feat: prepare preview deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,16 @@ export default defineNuxtConfig({
     devtools: {
         enabled: true
     },
-
     app: {
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1'
+        },
+        baseURL: '/preview'
+    },
+    nitro: {
+        prerender: {
+            routes: ["/"],
         },
     },
     css: [
