e6b7f04e487d2a4dfcfbcfa8918adda421ee255d
roxanafrunza <frunza_roxana@yahoo.com>
2025-02-24 21:49:39 +0200

    feat: extracurricular hero component
    -adding the extracurricular hero component
    - align the social media icons to vertical on laptops and horizontal on phones

--- pages/for-students.vue
+++ pages/for-students.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 
+import ExtracurricularHero from "~/components/students/extracurricular-hero.vue";
 </script>
 
 <template>
-
+    <extracurricular-hero/>
 </template>
 
 <style scoped>
