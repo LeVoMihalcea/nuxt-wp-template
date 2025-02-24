2a02400f0b2c90b4e43305cb262ef5e438881505
Leo Mihalcea <leomihalcea@gmail.com>
2025-02-24 22:45:48 +0200

    feat: create new pages

---
+++ pages/extra-curriculum.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import ExtracurricularHero from "~/components/students/extracurricular-hero.vue";
+</script>
+
+<template>
+    <extracurricular-hero/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/specializations.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+    import PreparedCard from "~/components/index/prepared-card.vue";
+    import PageContent from "~/layouts/page-content.vue";
+
+    const specializations = useSpecializations().data;
+</script>
+
+<template>
+    <generic-hero
+        :title="$t('specializations.hero.title')"
+        :subtitle="$t('specializations.hero.subtitle')"
+        :button="$t('specializations.hero.contactUs')"
+        background-image="/hero/hero-specializations.png"
+    />
+
+    <page-content>
+<!--        <div class="grid">-->
+<!--            <div class="p-6 max-h-3rem">-->
+<!--                <prepared-card-->
+<!--                    v-for="(specialization, index) in specializations"-->
+<!--                    :key="index"-->
+<!--                    :secondary="true"-->
+<!--                    :title="specialization.title"-->
+<!--                    :description="specialization.excerpt"-->
+<!--                    :image-url="specialization.imageUrl"-->
+<!--                />-->
+<!--            </div>-->
+<!--        </div>-->
+    </page-content>
+</template>
+
+<style scoped>
+</style>
