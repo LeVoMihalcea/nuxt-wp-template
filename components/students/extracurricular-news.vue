fbab80b3a2897c5240fc0cb6ff56446de32b1721
roxanafrunza <frunza_roxana@yahoo.com>
2025-02-25 11:55:20 +0200

    feat: extracurricular news component
    -adding the extracurricular news component

---
+++ components/students/extracurricular-news.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import PreparedCard from "~/components/index/prepared-card.vue";
+</script>
+
+<template>
+  <h1>{{ $t('extracurricular-news.title') }}</h1>
+  <prepared-card
+      :date="$t('extracurricular-news.date')"
+      :title="$t('extracurricular-news.title')"
+      :description="$t('extracurricular-news.text')"
+      :button="$t('extracurricular-news.button')"
+      background-image="/hero/hero-about.png"
+      />
+</template>
+
+<style scoped>
+
+</style>
