e9ca4e2166b87bb453c918535f3ce46315d658a5
Leo Mihalcea <leomihalcea@gmail.com>
2025-03-01 15:41:59 +0200

    feat: improve the error page

---
+++ error.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import type {NuxtError} from '#app'
+
+    const props = defineProps({
+        error: Object as () => NuxtError
+    })
+</script>
+
+<template>
+    <div class="m-8">
+        <h1>Something went wrong..</h1>
+        <pre class="text-gray">
+            {{ error?.statusCode }}
+            <br>
+            {{ error }}
+        </pre>
+
+        <NuxtLink to="/">
+            <Button>
+                Go back home
+            </Button>
+        </NuxtLink>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
